7b9840a7375d646013bbb616d10c7403392efaf0
Hetul3 <97708864+Hetul3@users.noreply.github.com>
2023-09-28 06:04:48 +0000

    questions for HD and OB

--- interface/src/HeartDisease.js
+++ interface/src/HeartDisease.js
@@ -5,6 +5,21 @@ function HeartDisease() {
     <div>
       <h1>Heart Disease Page</h1>
       <p>This is the Heart Disease page content.</p>
+
+
+      <p>Input your Age</p>
+      <p>Input you Sex</p>
+      <p>What is your Chest Pain Type</p>
+      <p>What is your Resting Blood Pressure</p>
+      <p>What is your Serum Cholestoral</p>
+      <p>Is your Fasting Blood Sugar {">"} 120 mg/dl</p>
+      <p>What are your Resting Electrocardiographic Results</p>
+      <p>Maximum Heart Rate Achieved Recently</p>
+      <p>Exercised Induced Angina</p>
+      <p>ST Depression Induced by Exercise Relative to Rest</p>
+      <p>The slope of the peak Exercise ST Segment</p>
+      <p>Number of Major Vessels Colored by Flouroscopy</p>
+      <p>Thal</p>
     </div>
   );
 }
--- interface/src/ObesityLevel.js
+++ interface/src/ObesityLevel.js
@@ -5,6 +5,23 @@ function ObesityLevel() {
     <div>
       <h1>Obesity Level Page</h1>
       <p>This is the Obesity Level page content.</p>
+
+      <p>What Is Your Sex</p>
+      <p>Input Your Age</p>
+      {/* could code in a quick converter for people */}
+      <p>Input Your Weight in kg</p> 
+      <p>What is Your Height in meters</p>
+      <p>Do you Have a Family History with Obesity</p>
+      <p>Do you Frequently Consume High-Calorie Meals?</p>
+      <p>Do you Have Frequent Consumption of Vegetables and Fruits</p>
+      <p>What are Your Number of Main Meals</p>
+      <p>Do you Smoke</p>
+      <p>Do you Consume Foods Between Meals</p>
+      <p>How Frequently do you Consume Alcohol</p>
+      <p>Do you Monitor Your Calories</p>
+      <p>What is Your Physical Activity per Week</p>
+      <p>What is Your Time Using Devices</p>
+      <p>What Transportation do you Mainly Use</p>
     </div>
   );
 }
