94496285f6a34750d5b98b96a4aa95d482bed97a
Hetul3 <97708864+Hetul3@users.noreply.github.com>
2023-11-12 23:32:16 -0500

    adding changes

--- interface/src/HeartDisease.js
+++ interface/src/HeartDisease.js
@@ -20,6 +20,234 @@ function HeartDisease() {
 
   const [predictedLabel, setPredictedLabel] = useState(10);
   const [data, setData] = useState({});
+  const [currentForm, setCurrentForm] = useState(1);
+  const MAX_FORMS = 13;
+
+  const handleNext = () => {
+    setCurrentForm((prevForm) => prevForm + 1);
+  };
+
+  const renderForm = () => {
+    switch (currentForm) {
+      case 1:
+        return (
+          <div>
+            <p>Input your Age</p>
+            <input
+              type="number"
+              name="h_age"
+              placeholder="Enter a value"
+              value={inputValuesH.h_age}
+              onChange={handleInputChange}
+              min="0"
+              max="100"
+            />
+            {/* ... other inputs for Form 1 */}
+          </div>
+        );
+      case 2:
+        return (
+          <div>
+            <p>Input your Sex</p>
+            <select
+              name="h_sex"
+              value={inputValuesH.h_sex}
+              onChange={handleInputChange}
+            >
+              <option value={0}>Female</option>
+              <option value={1}>Male</option>
+            </select>
+            {/* ... other inputs for Form 2 */}
+          </div>
+        );
+      case 3:
+        return (
+          <div>
+            <p>What is your Chest Pain Type</p>
+            <select
+              name="h_cp"
+              value={inputValuesH.h_cp}
+              onChange={handleInputChange}
+            >
+              <option value={1}>Typical Angina</option>
+              <option value={2}>Atypical Angina</option>
+              <option value={3}>Non-Anginal Pain</option>
+              <option value={4}>Asympotomatic</option>
+            </select>
+            {/* ... other inputs for Form 1 */}
+          </div>
+        );
+      case 4:
+        return (
+          <div>
+            <p>What is your Resting Blood Pressure {"(Systolic)"}</p>
+            <input
+              type="number"
+              name="h_trestbps"
+              placeholder="Enter a value"
+              value={inputValuesH.h_trestbps}
+              onChange={handleInputChange}
+              min="0"
+              max="200"
+            />
+            {/* ... other inputs for Form 1 */}
+          </div>
+        );
+      case 5:
+        return (
+          <div>
+            <p>What is your Serum Cholestoral {"(Triglycerides)"}</p>
+            <input
+              type="number"
+              name="h_chol"
+              placeholder="Enter a value"
+              value={inputValuesH.h_chol}
+              onChange={handleInputChange}
+              min="70"
+              max="600"
+            />
+
+            {/* ... other inputs for Form 1 */}
+          </div>
+        );
+      case 6:
+        return (
+          <div>
+            <p>Is your Fasting Blood Sugar {">"} 120 mg/dl</p>
+            <select
+              name="h_fbs"
+              value={inputValuesH.h_fbs}
+              onChange={handleInputChange}
+            >
+              <option value={0}>No</option>
+              <option value={1}>Yes</option>
+            </select>
+
+            {/* ... other inputs for Form 1 */}
+          </div>
+        );
+      case 7:
+        return (
+          <div>
+            <p>What are your Resting Electrocardiographic Results</p>
+            <select
+              name="h_restecg"
+              value={inputValuesH.h_restecg}
+              onChange={handleInputChange}
+            >
+              <option value={0}>Normal</option>
+              <option value={1}>ST-T Wave Abnormality</option>
+              <option value={2}>
+                Showing Probable or Definit Left Ventricular Hypertropy by
+                Estes' Criteria
+              </option>
+            </select>
+            {/* ... other inputs for Form 1 */}
+          </div>
+        );
+      case 8:
+        return (
+          <div>
+            <p>Maximum Heart Rate Achieved Recently</p>
+            <input
+              type="number"
+              name="h_thalach"
+              placeholder="Enter a value"
+              value={inputValuesH.h_thalach}
+              onChange={handleInputChange}
+              min="20"
+              max="220"
+            />
+
+            {/* ... other inputs for Form 1 */}
+          </div>
+        );
+      case 9:
+        return (
+          <div>
+            <p>Exercised Induced Angina</p>
+            <select
+              name="h_exang"
+              value={inputValuesH.h_exang}
+              onChange={handleInputChange}
+            >
+              <option value={0}>No</option>
+              <option value={1}>Yes</option>
+            </select>
+            {/* ... other inputs for Form 1 */}
+          </div>
+        );
+      case 10:
+        return (
+          <div>
+            <p>ST Depression Induced by Exercise Relative to Rest</p>
+            <input
+              type="number"
+              step="0.1"
+              name="h_oldpeak"
+              placeholder="Enter a value"
+              value={inputValuesH.h_oldpeak}
+              onChange={handleInputChange}
+              min="0"
+              max="3"
+            />
+            {/* ... other inputs for Form 1 */}
+          </div>
+        );
+      case 11:
+        return (
+          <div>
+            <p>The slope of the peak Exercise ST Segment</p>
+            <select
+              name="h_slope"
+              value={inputValuesH.h_slope}
+              onChange={handleInputChange}
+            >
+              <option value={1}>Unsloping</option>
+              <option value={2}>Flat</option>
+              <option value={3}>Downsloping</option>
+            </select>
+            {/* ... other inputs for Form 1 */}
+          </div>
+        );
+      case 12:
+        return (
+          <div>
+            <p>Number of Major Vessels Colored by Flouroscopy</p>
+            <input
+              type="number"
+              name="h_ca"
+              placeholder="Enter a value"
+              value={inputValuesH.h_ca}
+              onChange={handleInputChange}
+              min="0"
+              max="3"
+            />
+            {/* ... other inputs for Form 1 */}
+          </div>
+        );
+      case 13:
+        return (
+          <div>
+            <p>Thal</p>
+            <select
+              name="h_thal"
+              value={inputValuesH.h_thal}
+              onChange={handleInputChange}
+            >
+              <option value={3}>Normal</option>
+              <option value={6}>Fixed Defect</option>
+              <option value={7}>Reversible Defect</option>
+            </select>
+            {/* ... other inputs for Form 1 */}
+          </div>
+        );
+
+      default:
+        return null;
+    }
+  };
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -104,7 +332,7 @@ function HeartDisease() {
     let parsedValue;
     if (type === "number") {
       parsedValue =
-        name === 'h_oldpeak' ? parseFloat(value) : parseInt(value, 10);
+        name === "h_oldpeak" ? parseFloat(value) : parseInt(value, 10);
     } else {
       parsedValue = parseInt(value, 10);
     }
@@ -115,6 +343,163 @@ function HeartDisease() {
     }));
   };
 
+  //   return (
+  //     <div>
+  //       <h1>Heart Disease Page</h1>
+  //       <h1>{data.members}</h1>
+  //       <p>This is the Heart Disease page content.</p>
+  //       <p>Predicted class: {predictedLabel}</p>
+
+  //       <p>Input your Age</p>
+  //       <input
+  //         type="number"
+  //         name="h_age"
+  //         placeholder="Enter a value"
+  //         value={inputValuesH.h_age}
+  //         onChange={handleInputChange}
+  //         min="0"
+  //         max="100"
+  //       />
+
+  //       <p>Input you Sex</p>
+  //       <select
+  //         name="h_sex"
+  //         value={inputValuesH.h_sex}
+  //         onChange={handleInputChange}
+  //       >
+  //         <option value={0}>Female</option>
+  //         <option value={1}>Male</option>
+  //       </select>
+
+  //       <p>What is your Chest Pain Type</p>
+  //       <select
+  //         name="h_cp"
+  //         value={inputValuesH.h_cp}
+  //         onChange={handleInputChange}
+  //       >
+  //         <option value={1}>Typical Angina</option>
+  //         <option value={2}>Atypical Angina</option>
+  //         <option value={3}>Non-Anginal Pain</option>
+  //         <option value={4}>Asympotomatic</option>
+  //       </select>
+
+  //       <p>What is your Resting Blood Pressure {"(Systolic)"}</p>
+  //       <input
+  //         type="number"
+  //         name="h_trestbps"
+  //         placeholder="Enter a value"
+  //         value={inputValuesH.h_trestbps}
+  //         onChange={handleInputChange}
+  //         min="0"
+  //         max="200"
+  //       />
+
+  //       <p>What is your Serum Cholestoral {"(Triglycerides)"}</p>
+  //       <input
+  //         type="number"
+  //         name="h_chol"
+  //         placeholder="Enter a value"
+  //         value={inputValuesH.h_chol}
+  //         onChange={handleInputChange}
+  //         min="70"
+  //         max="600"
+  //       />
+
+  //       <p>Is your Fasting Blood Sugar {">"} 120 mg/dl</p>
+  //       <select
+  //         name="h_fbs"
+  //         value={inputValuesH.h_fbs}
+  //         onChange={handleInputChange}
+  //       >
+  //         <option value={0}>No</option>
+  //         <option value={1}>Yes</option>
+  //       </select>
+
+  //       <p>What are your Resting Electrocardiographic Results</p>
+  //       <select
+  //         name="h_restecg"
+  //         value={inputValuesH.h_restecg}
+  //         onChange={handleInputChange}
+  //       >
+  //         <option value={0}>Normal</option>
+  //         <option value={1}>ST-T Wave Abnormality</option>
+  //         <option value={2}>
+  //           Showing Probable or Definit Left Ventricular Hypertropy by Estes'
+  //           Criteria
+  //         </option>
+  //       </select>
+
+  //       <p>Maximum Heart Rate Achieved Recently</p>
+  //       <input
+  //         type="number"
+  //         name="h_thalach"
+  //         placeholder="Enter a value"
+  //         value={inputValuesH.h_thalach}
+  //         onChange={handleInputChange}
+  //         min="20"
+  //         max="220"
+  //       />
+
+  //       <p>Exercised Induced Angina</p>
+  //       <select
+  //         name="h_exang"
+  //         value={inputValuesH.h_exang}
+  //         onChange={handleInputChange}
+  //       >
+  //         <option value={0}>No</option>
+  //         <option value={1}>Yes</option>
+  //       </select>
+
+  //       <p>ST Depression Induced by Exercise Relative to Rest</p>
+  //       <input
+  //         type="number"
+  //         step="0.1"
+  //         name="h_oldpeak"
+  //         placeholder="Enter a value"
+  //         value={inputValuesH.h_oldpeak}
+  //         onChange={handleInputChange}
+  //         min="0"
+  //         max="3"
+  //       />
+
+  //       <p>The slope of the peak Exercise ST Segment</p>
+  //       <select
+  //         name="h_slope"
+  //         value={inputValuesH.h_slope}
+  //         onChange={handleInputChange}
+  //       >
+  //         <option value={1}>Unsloping</option>
+  //         <option value={2}>Flat</option>
+  //         <option value={3}>Downsloping</option>
+  //       </select>
+
+  //       <p>Number of Major Vessels Colored by Flouroscopy</p>
+  //       <input
+  //         type="number"
+  //         name="h_ca"
+  //         placeholder="Enter a value"
+  //         value={inputValuesH.h_ca}
+  //         onChange={handleInputChange}
+  //         min="0"
+  //         max="3"
+  //       />
+
+  //       <p>Thal</p>
+  //       <select
+  //         name="h_thal"
+  //         value={inputValuesH.h_thal}
+  //         onChange={handleInputChange}
+  //       >
+  //         <option value={3}>Normal</option>
+  //         <option value={6}>Fixed Defect</option>
+  //         <option value={7}>Reversible Defect</option>
+  //       </select>
+
+  //       <button onClick={handleSubmit}>Submit</button>
+  //       <p>Predicted Obesity Level: {predictedLabel}</p>
+  //     </div>
+  //   );
+  // }
   return (
     <div>
       <h1>Heart Disease Page</h1>
@@ -122,150 +507,9 @@ function HeartDisease() {
       <p>This is the Heart Disease page content.</p>
       <p>Predicted class: {predictedLabel}</p>
 
-      <p>Input your Age</p>
-      <input
-        type="number"
-        name="h_age"
-        placeholder="Enter a value"
-        value={inputValuesH.h_age}
-        onChange={handleInputChange}
-        min="0"
-        max="100"
-      />
+      {renderForm()}
 
-      <p>Input you Sex</p>
-      <select
-        name="h_sex"
-        value={inputValuesH.h_sex}
-        onChange={handleInputChange}
-      >
-        <option value={0}>Female</option>
-        <option value={1}>Male</option>
-      </select>
-
-      <p>What is your Chest Pain Type</p>
-      <select
-        name="h_cp"
-        value={inputValuesH.h_cp}
-        onChange={handleInputChange}
-      >
-        <option value={1}>Typical Angina</option>
-        <option value={2}>Atypical Angina</option>
-        <option value={3}>Non-Anginal Pain</option>
-        <option value={4}>Asympotomatic</option>
-      </select>
-
-      <p>What is your Resting Blood Pressure {"(Systolic)"}</p>
-      <input
-        type="number"
-        name="h_trestbps"
-        placeholder="Enter a value"
-        value={inputValuesH.h_trestbps}
-        onChange={handleInputChange}
-        min="0"
-        max="200"
-      />
-
-      <p>What is your Serum Cholestoral {"(Triglycerides)"}</p>
-      <input
-        type="number"
-        name="h_chol"
-        placeholder="Enter a value"
-        value={inputValuesH.h_chol}
-        onChange={handleInputChange}
-        min="70"
-        max="600"
-      />
-
-      <p>Is your Fasting Blood Sugar {">"} 120 mg/dl</p>
-      <select
-        name="h_fbs"
-        value={inputValuesH.h_fbs}
-        onChange={handleInputChange}
-      >
-        <option value={0}>No</option>
-        <option value={1}>Yes</option>
-      </select>
-
-      <p>What are your Resting Electrocardiographic Results</p>
-      <select
-        name="h_restecg"
-        value={inputValuesH.h_restecg}
-        onChange={handleInputChange}
-      >
-        <option value={0}>Normal</option>
-        <option value={1}>ST-T Wave Abnormality</option>
-        <option value={2}>
-          Showing Probable or Definit Left Ventricular Hypertropy by Estes'
-          Criteria
-        </option>
-      </select>
-
-      <p>Maximum Heart Rate Achieved Recently</p>
-      <input
-        type="number"
-        name="h_thalach"
-        placeholder="Enter a value"
-        value={inputValuesH.h_thalach}
-        onChange={handleInputChange}
-        min="20"
-        max="220"
-      />
-
-      <p>Exercised Induced Angina</p>
-      <select
-        name="h_exang"
-        value={inputValuesH.h_exang}
-        onChange={handleInputChange}
-      >
-        <option value={0}>No</option>
-        <option value={1}>Yes</option>
-      </select>
-
-      <p>ST Depression Induced by Exercise Relative to Rest</p>
-      <input
-        type="number"
-        step="0.1"
-        name="h_oldpeak"
-        placeholder="Enter a value"
-        value={inputValuesH.h_oldpeak}
-        onChange={handleInputChange}
-        min="0"
-        max="3"
-      />
-
-      <p>The slope of the peak Exercise ST Segment</p>
-      <select
-        name="h_slope"
-        value={inputValuesH.h_slope}
-        onChange={handleInputChange}
-      >
-        <option value={1}>Unsloping</option>
-        <option value={2}>Flat</option>
-        <option value={3}>Downsloping</option>
-      </select>
-
-      <p>Number of Major Vessels Colored by Flouroscopy</p>
-      <input
-        type="number"
-        name="h_ca"
-        placeholder="Enter a value"
-        value={inputValuesH.h_ca}
-        onChange={handleInputChange}
-        min="0"
-        max="3"
-      />
-
-      <p>Thal</p>
-      <select
-        name="h_thal"
-        value={inputValuesH.h_thal}
-        onChange={handleInputChange}
-      >
-        <option value={3}>Normal</option>
-        <option value={6}>Fixed Defect</option>
-        <option value={7}>Reversible Defect</option>
-      </select>
+      {currentForm < MAX_FORMS && <button onClick={handleNext}>Next</button>}
 
       <button onClick={handleSubmit}>Submit</button>
       <p>Predicted Obesity Level: {predictedLabel}</p>
